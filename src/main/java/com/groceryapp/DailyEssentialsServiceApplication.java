9c71a8b269e2cc39047f50c3ee7bfc4a6ea56b70
Shubham <patilshubham269@gmail.com>
2025-03-13 01:31:10 +0530

    Configurations

---
+++ src/main/java/com/groceryapp/DailyEssentialsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.groceryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyEssentialsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyEssentialsServiceApplication.class, args);
+    }
+
+}
